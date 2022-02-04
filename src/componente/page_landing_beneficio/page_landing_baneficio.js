e7019d24333febaa970a9086b9f53c63f8929aef
Bárbara Ann Lima <babiplima@gmail.com>
2022-02-04 16:06:00 -0300

    Criar construtor page_landing_beneficio

--- src/componente/page_landing_beneficio/page_landing_baneficio.js
+++ src/componente/page_landing_beneficio/page_landing_baneficio.js
@@ -1,3 +1,12 @@
 import react,{Component}from"react";
 import { Form, FormGroup, Label, Input, Button,Alert } from "reactstrap";
 import Header from "../header";
+
+export default class page_landing_beneficio extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            message: this.props.state ? this.props.state.message :"",
+        };
+    }
+}
