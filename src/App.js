23f8745055fcdf1d192b783c53dee061562ae89c
Bárbara Ann Lima <babiplima@gmail.com>
2022-02-04 16:51:43 -0300

    Criar rota App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
 import react, {Component} from "react";
 import "./App.css"
 import Routes from "./route";
+
+export default class App extends Component{
+    render(){
+          return(
+            <div className="App"> 
+               <Routes/>
+            </div>
+          );
+        }
+    }
