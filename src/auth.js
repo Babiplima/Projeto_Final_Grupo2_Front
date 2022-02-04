1845cd6aa89deffd0f6430c1af7a985542753fce
Bárbara Ann Lima <babiplima@gmail.com>
2022-02-04 17:15:15 -0300

    Criar RotaPrivada em auth.js

--- src/auth.js
+++ src/auth.js
@@ -1,3 +1,25 @@
 import react from "react";
 
 import { Route, Redirect } from "react-router-dom";
+
+
+const PrivateRoute = ({component: Component, ...rest}) => {
+    return (
+        <Route 
+            {...rest}
+            render={props => 
+            isAuth() ? (
+                <Component {...props} />
+            ): (
+                <Redirect 
+                    to={{
+                        pathname: '/',
+                        state: { message: 'Usuário não autorizado' }
+                    }}
+                />
+            )}
+        />
+    );
+}
+
+export default PrivateRoute;
