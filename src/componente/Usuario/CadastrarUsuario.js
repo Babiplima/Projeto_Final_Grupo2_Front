fd7863585b978a52bf0968a520676a624032ddaf
Larissa Novais <larinovais54@gmail.com>
2022-02-07 11:40:59 -0300

    Refactor: Import

--- src/componente/Usuario/CadastrarUsuario.js
+++ src/componente/Usuario/CadastrarUsuario.js
@@ -1,6 +1,26 @@
-import react, {Component} from "react";
-import {Form, FormGroup, Label, Input, Button, Alert} from 'reactstrap'
+import React, {Component} from "react";
+import {Form, FormGroup, Label, Input, Button} from "reactstrap"
+import Header from "../Header/Header";
+
 
 export default class CadastrarUsuario extends Component {
-
+    render() {
+        return(
+            <div>
+                <Header title = "Cadastrar Usuário"/>
+              <hr/>
+                <Form>
+                        <FormGroup>
+                            <Label for = "email"> Email </Label>
+                            <Input type= "text" id= "email" placeholder="Informe seu email: "/>
+                        </FormGroup>
+                    <FormGroup>
+                        <Label for = "password"> Senha </Label>
+                        <Input type= "password" id= "password" placeholder="Informe sua senha: "/>
+                    </FormGroup>
+                    <Button color="primary"> Enviar </Button>
+                 </Form>  
+            </div>
+      );
+    }
 }
