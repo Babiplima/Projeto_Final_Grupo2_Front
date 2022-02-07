f46c3a31ca6dcfe20e3a51ae8296707194d6dd9a
Larissa Novais <larinovais54@gmail.com>
2022-02-07 15:30:40 -0300

    Feat: adicinar cadastrar usuario

--- src/App.js
+++ src/App.js
@@ -12,18 +12,3 @@ export default class App extends Component {
         );
     }
 }
-
-import React from "react";
-import Login from "./componente/Usuario/login";
-import './App.css'
-
-
-function App() {
- return(
-    <div className="App">
-        <Login/>
-    </div>
- );
-}
-
-export default App;
