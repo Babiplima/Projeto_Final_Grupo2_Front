056cabfc0ddaddf4c99a788d7bed09e81de6a69c
Larissa Novais <larinovais54@gmail.com>
2022-02-07 13:43:29 -0300

    ajustes

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import "./App.css"
+import CadastrarUsuario from "./componente/Usuario/CadastrarUsuario";
+
+
+export default class App extends Component {
+    render(){
+        return(
+           <div className="App">
+             <CadastrarUsuario/>
+           </div>
+        );
+    }
+}
