13dcf8ad82986be4fd58e9c1031f56944a6da4f0
Larissa Novais <larinovais54@gmail.com>
2022-02-14 17:02:52 -0300

    Adicionar rotas

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,10 @@ import CadastrarUsuario from './componente/Usuario/CadastrarUsuario';
 import DashboardUsuario from './componente/Usuario/DashboardUsuario'
 import CadastrarFuncionario from './componente/Funcionario/CadastrarFuncionario';
 import DashboardFuncionario from './componente/Funcionario/DashboardFuncionario';
+import Beneficio from './componente/Beneficio/beneficio';
+import DashboardBeneficio from './componente/Beneficio/dashboardBeneficio';
+import DashboardAtividadeFisica from './componente/Atividade/dashboardAtividadeFisica';
+import Atividade from './componente/Atividade/atividade';
 
 const Routes = () => (
     <Router>
@@ -15,6 +19,10 @@ const Routes = () => (
             <PrivateRoute path='/dashboardFuncionario' component={DashboardFuncionario}/>
             <PrivateRoute path='/cadastrarFuncionario' component={CadastrarFuncionario}/>
             <Route exact path='/cadastrarUsuario' component={CadastrarUsuario}/>
+            <Route exact path='/beneficio' component={Beneficio} />
+            <Route exact path='/dashboardBeneficio' component={DashboardBeneficio} />
+            <Route exact path='/atividade' component={Atividade} />
+            <Route exact path='/dashboardAtividade' component={DashboardAtividadeFisica} />
         </Switch>
     </Router>
 
